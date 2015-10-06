c142b5be01e6b353be11d6b172137de654572b65
upbeatvision <info@upbeatvision.com>
2015-10-06 00:54:13 +0000

    lesson9

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ActiveRecord::Base
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
     # 内容は必須入力かつ2文字以上30文字以下
-    validates :age , only_integer: true, greater_than_or_equal_to: 0 , presence: true
+    validates :age , numericality: {only_integer: true, greater_than_or_equal_to: 0 } , presence: true
 end
